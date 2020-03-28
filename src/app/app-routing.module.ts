95751f61e469eb2d0f846f5410037aaa23e70f27
Tara Z. Manicsic <tzmanics@gmail.com>
2020-03-28 00:18:16 -0400

    adds generated modules and routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,29 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    loadChildren: () => import("./home/home.module").then(m => m.HomeModule)
+  },
+  {
+    path: "about",
+    loadChildren: () => import("./about/about.module").then(m => m.AboutModule)
+  },
+  {
+    path: "contact",
+    loadChildren: () =>
+      import("./contact/contact.module").then(m => m.ContactModule)
+  },
+  {
+    path: "resources",
+    loadChildren: () =>
+      import("./resources/resources.module").then(m => m.ResourcesModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
